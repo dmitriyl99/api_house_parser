60904a6bff6011377aebf123c3664624e9ec62e4
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-03-07 15:21:43 +0500

    Refactor

--- app/extractor/strategies/__init__.py
+++ app/extractor/strategies/__init__.py
@@ -2,8 +2,9 @@ from abc import ABC
 from typing import List, Generator
 
 from app.dal.models import Building
+from app.extractor.viewmodels import BuildingViewModel
 
 
 class BuildingExtractionStrategy(ABC):
-    def extract(self) -> Generator[List[Building], None, None]:
+    def extract(self) -> Generator[List[BuildingViewModel], None, None]:
         raise NotImplementedError("Subclasses must implement the extract method.")
--- app/scheduler/jobs.py
+++ app/scheduler/jobs.py
@@ -1,5 +1,7 @@
 from app.extractor import BuildingExtractor
 from app.extractor.strategies.uybor import UyBorExtractionStrategy
+from app.extractor.strategies.olx import OlxExtractionStrategy
+from app.extractor.viewmodels import BuildingViewModel
 from app.dal.repositories import buildings as buildings_repository
 from app.dal.models import Building, Image
 
@@ -8,23 +10,34 @@ def parse_uybor():
     extractor = BuildingExtractor(UyBorExtractionStrategy())
     for buildings in extractor.extract():
         for building in buildings:
-            buildings_repository.save_building(Building(
-                territory=building.territory,
-                area=building.area,
-                sell_type=building.sell_type,
-                room_number=building.room_number,
-                land_area=building.land_area,
-                building_area=building.building_area,
-                price=building.price,
-                floor=building.floor,
-                floor_number=building.floor_number,
-                building_repair=building.building_repair,
-                type_of_ad=building.type_of_ad,
-                source=building.source,
-                views=building.views,
-                user_name=building.user_name,
-                user_phone=building.user_phone
-            ), list(map(lambda x: Image(
-                filename=x.filename,
-                url=x.url
-            ), building.images)))
+            _save_building_to_repository(building)
+
+
+def parse_olx():
+    extractor = BuildingExtractor(OlxExtractionStrategy())
+    for buildings in extractor.extract():
+        for building in buildings:
+            _save_building_to_repository(building)
+
+
+def _save_building_to_repository(building: BuildingViewModel):
+    buildings_repository.save_building(Building(
+        territory=building.territory,
+        area=building.area,
+        sell_type=building.sell_type,
+        room_number=building.room_number,
+        land_area=building.land_area,
+        building_area=building.building_area,
+        price=building.price,
+        floor=building.floor,
+        floor_number=building.floor_number,
+        building_repair=building.building_repair,
+        type_of_ad=building.type_of_ad,
+        source=building.source,
+        views=building.views,
+        user_name=building.user_name,
+        user_phone=building.user_phone
+    ), list(map(lambda x: Image(
+        filename=x.filename,
+        url=x.url
+    ), building.images)))
