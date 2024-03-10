a11252682c50e61ad59234d37a36dd3d2e22a705
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-03-10 11:19:43 +0500

    add parse olx to scheduler

--- app/scheduler/__init__.py
+++ app/scheduler/__init__.py
@@ -1,13 +1,15 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
-from .jobs import parse_uybor
+from .jobs import parse_uybor, parse_olx
 
 
 _scheduler = BackgroundScheduler()
 
+
 def start():
     _scheduler.add_job(parse_uybor, trigger=IntervalTrigger(minutes=1), id='parse_uybor', replace_existing=True)
+    _scheduler.add_job(parse_olx, trigger=IntervalTrigger(minutes=30), id='parse_olx', replace_existing=True)
     _scheduler.start()
 
 
