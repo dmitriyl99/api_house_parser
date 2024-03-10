28aa05ca093536656e4b13ea5624c9657df4f79f
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-03-10 11:19:51 +0500

    fix nullable values

--- app/extractor/viewmodels.py
+++ app/extractor/viewmodels.py
@@ -15,15 +15,15 @@ class BuildingViewModel(BaseModel):
     sell_type: str
     room_number: int | None = None
     land_area: float | None = None
-    building_area: float
+    building_area: float | None
     price: int
     floor: int | None = None
-    floor_number: int
-    building_repair: str
+    floor_number: int | None = None
+    building_repair: str | None = None
     type_of_ad: str
     source: str
     views: int | None = None
-    user_name: str
+    user_name: str | None = None
     user_phone: str | None = None
     category_id: int | None = None
 
