0452b3b9cebc29f0bda16f350df1e3670a812cf8
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-03-26 21:52:11 +0500

    check response for list

--- set_title_and_description_olx.py
+++ set_title_and_description_olx.py
@@ -12,6 +12,8 @@ for building in buildings:
     response = requests.get(f'https://www.olx.uz/api/v1/offers/{building.olx_id}')
     if response.status_code == 410:
         continue
+    if type(response.json()) is list:
+        continue
     data = response.json()['data']
     building_repository.set_title_and_description(
         building.id, data['title'], re.sub(clean_html_regex, '', data['description'])
